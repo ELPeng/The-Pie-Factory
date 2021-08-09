b3d40bc7619bdb4ab3309d1d59afcd7e9aa43ffb
EPeng <erpengic@gmail.com>
2021-08-09 13:23:35 -0500

    completed FAQs accordian style

--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -1,42 +1,140 @@
-import IconButton from '@material-ui/core/IconButton'
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Faqs(){
-    const btns = document.querySelectorAll('.question-btn')
-    console.log(btns)
 
+    // const useStyles = makeStyles((theme) => ({
+    //     heading: {
+    //         fontSize: theme.typography.pxToRem(18),
+    //         fontWeight: theme.typography.fontWeightBold,
+    //     },
+    //     tabStyle: {
+    //         padding: "10px 0",
+    //         backgroundColor="#c2b59b",
+    //     }
+    //   }));
+    // const classes = useStyles();
     return(
         <>
             <div className="page-banner">
                 <h2>Frequently Asked Questions</h2>
             </div>
-            <div className='main-page'>
-                <article className="question">
-                    <div className="question-title">
-                        <p className="page-text"><b>How do I place an order?</b></p>
-                        <IconButton className="show-btn">
-                            <KeyboardArrowDownIcon />
-                        </IconButton>
-                        <IconButton className="hide-btn">
-                            <KeyboardArrowUpIcon />
-                        </IconButton>
+            <div className="main-page">
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header">
+                        <p className="page-text">How do I place an order?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            Orders MUST be in by 12:00 noon, the day before scheduled delivery. Late orders or un-scheduled deliveries may incur extra delivery charges. Call for more information.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
 
-                    </div>
-                    <div className="question-text">
-                        <p >Please call (713) 666-8501 to place your order. You may pick up your order at our location, 5611 Bellaire Blvd. Suite 128, Houston, TX 77081. Orders MUST be in by 12:00 noon, the day before scheduled delivery. Late orders or un-scheduled deliveries may incur extra delivery charges. Call for more information.</p>
-                    </div>
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="panel2a-header">
+                        <p className="page-text">Does the Pie Factory have pies available for same day walk-ins?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            Yes, but our selection of pies that we have on hand is very limited. If you have a specific pie you would like, please call the day before and we will make it fresh for you!
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
 
-                </article>
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3a-content"
+                    id="panel3a-header">
+                        <p className="page-text">Does the Pie Factory sell to retail customers?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            Yes, but we highly recommend calling in at least one day prior to place your order.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
 
-                
-                <p className="page-text"><b>How do I place an order?</b></p>
-                <p className="page-text">Orders MUST be in by 12:00 noon, the day before scheduled delivery. Late orders or un-scheduled deliveries may incur extra delivery charges. Call for more information.</p>
-                <p className="page-text"><b>How do I place an order?</b></p>
-                <p className="page-text">Orders MUST be in by 12:00 noon, the day before scheduled delivery. Late orders or un-scheduled deliveries may incur extra delivery charges. Call for more information.</p>
-                <p className="page-text"><b>How do I place an order?</b></p>
-                <p className="page-text">Orders MUST be in by 12:00 noon, the day before scheduled delivery. Late orders or un-scheduled deliveries may incur extra delivery charges. Call for more information.</p>
-            </div>
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel4a-content">
+                        <p className="page-text">Does the Pie Factory deliver?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            We do not offer deliveries to residential or non-wholesale customers unless the orders are particularly large.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel5a-content"
+                    id="panel5a-header">
+                        <p className="page-text">Does the Pie Factory sell to retail customers</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            Orders MUST be in by 12:00 noon, the day before scheduled delivery. Late orders or un-scheduled deliveries may incur extra delivery charges. Call for more information.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel6a-content"
+                    id="panel6a-header">
+                        <p className="page-text">How do I get quotes or place an order for a specialty cake?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            Send us an email with a picture of the cake, the date you need the cake for, how many servings, and any additional customizations and we will send back a price quote.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel7a-content"
+                    id="panel7a-header">
+                        <p className="page-text">What forms of payment do you accept?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                            We accept cash and credit cards (AMEX, Visa, and Discover) as payment.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion className="acc-btn">
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel8a-content"
+                    id="panel8a-header">
+                        <p className="page-text">Does the Pie Factory make special recipes if I wanted to provide my own recipe?</p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p className="acc-text">
+                        We try our best to accommodate custom recipes, as long as we believe we can make a good product.
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
+        </div>
     </>
     )
 }
