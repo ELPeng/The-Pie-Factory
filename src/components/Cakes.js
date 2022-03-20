9b3516aabbddb6287b985836c0f247eae586099d
EPeng <erpengic@gmail.com>
2022-03-20 15:04:30 -0500

    updated pricing

--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -1,224 +1,243 @@
-import { useState } from 'react'
-import { Button, ButtonGroup } from '@material-ui/core'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Button, ButtonGroup } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Cakes = () => {
-    const [cakeType, setCakeType] = useState('white')
-    const cakes = [
-        {
-            title: '8" Round',
-            serving: 'Serves 8 - 16',
-            whitePrice: '24.50',
-            chocPrice: '25.50',
-            carrotPrice: '26.50'
-        },
-        {
-            title: '1/4 Sheet',
-            serving: 'Serves 12 - 20',
-            whitePrice: '27.50',
-            chocPrice: '28.50',
-            carrotPrice: '29.50'
-        },
-        {
-            title: '1/2 Sheet',
-            serving: 'Serves 18 - 40',
-            whitePrice: '51.50',
-            chocPrice: '53.50',
-            carrotPrice: '55.50'
-        },
-        {
-            title: 'Full Sheet',
-            serving: 'Serves 48-96',
-            whitePrice: '91.50',
-            chocPrice: '94.50',
-            carrotPrice: '99.50'
-        },
-    ]
+  const [cakeType, setCakeType] = useState("white");
+  const cakes = [
+    {
+      title: '8" Round',
+      serving: "Serves 8 - 16",
+      whitePrice: "25.50",
+      chocPrice: "26.50",
+      carrotPrice: "27.50",
+    },
+    {
+      title: "1/4 Sheet",
+      serving: "Serves 12 - 20",
+      whitePrice: "28.50",
+      chocPrice: "29.50",
+      carrotPrice: "30.50",
+    },
+    {
+      title: "1/2 Sheet",
+      serving: "Serves 18 - 40",
+      whitePrice: "52.50",
+      chocPrice: "54.50",
+      carrotPrice: "56.50",
+    },
+    {
+      title: "Full Sheet",
+      serving: "Serves 48-96",
+      whitePrice: "91.50",
+      chocPrice: "94.50",
+      carrotPrice: "99.50",
+    },
+  ];
 
-    // Changes the cake price based on the selected cake type
-    function selectCakePrice(cake){
-        switch(cakeType){
-            case 'white':
-                return cake.whitePrice
-            case 'chocolate':
-                return cake.chocPrice
-            case 'carrot':
-                return cake.carrotPrice
-        }
+  // Changes the cake price based on the selected cake type
+  function selectCakePrice(cake) {
+    switch (cakeType) {
+      case "white":
+        return cake.whitePrice;
+      case "chocolate":
+        return cake.chocPrice;
+      case "carrot":
+        return cake.carrotPrice;
     }
+  }
 
-    // Updates the cake card and button colors based on type of Cake
-    let labelStyle, priceContainerStyle, whiteButtonStyle, chocButtonStyle, carrotButtonStyle
-    if (cakeType == 'carrot'){
-        labelStyle = {
-            background: '#ED7014'
-        }
-        priceContainerStyle = {
-            background: 'rgba(255,165,0, 0.2)',
-        }
-        carrotButtonStyle = {
-            background: 'rgba(255,165,0, 0.2)'
-        }
-    }
-    else if(cakeType == 'chocolate'){
-        labelStyle = {
-            background: '#65350F'
-        }
-        priceContainerStyle = {
-            background: 'rgba(165, 42, 42, 0.2)',
-        }
-        chocButtonStyle = {
-            background: 'rgba(165, 42, 42, 0.2)'
-        }
-    }
-    else{
-        labelStyle = {
-            background: '#565656'
-        }
-        priceContainerStyle = {
-            background: 'rgba(255,253,208, 0.8)'
-        }
-        whiteButtonStyle = {
-            background: 'rgba(255,253,208, 0.8)'
-        }
-    }
+  // Updates the cake card and button colors based on type of Cake
+  let labelStyle,
+    priceContainerStyle,
+    whiteButtonStyle,
+    chocButtonStyle,
+    carrotButtonStyle;
+  if (cakeType == "carrot") {
+    labelStyle = {
+      background: "#ED7014",
+    };
+    priceContainerStyle = {
+      background: "rgba(255,165,0, 0.2)",
+    };
+    carrotButtonStyle = {
+      background: "rgba(255,165,0, 0.2)",
+    };
+  } else if (cakeType == "chocolate") {
+    labelStyle = {
+      background: "#65350F",
+    };
+    priceContainerStyle = {
+      background: "rgba(165, 42, 42, 0.2)",
+    };
+    chocButtonStyle = {
+      background: "rgba(165, 42, 42, 0.2)",
+    };
+  } else {
+    labelStyle = {
+      background: "#565656",
+    };
+    priceContainerStyle = {
+      background: "rgba(255,253,208, 0.8)",
+    };
+    whiteButtonStyle = {
+      background: "rgba(255,253,208, 0.8)",
+    };
+  }
 
-    const textStyle = {
-        fontSize: '1.2em',
-        fontStyle: 'italic'
-    }
-    
-    return (
-        <> 
-            <div className="products-cat-title">
-                <h2>Cakes</h2>
+  const textStyle = {
+    fontSize: "1.2em",
+    fontStyle: "italic",
+  };
+
+  return (
+    <>
+      <div className="products-cat-title">
+        <h2>Cakes</h2>
+      </div>
+      <div className="decorated-cakes-container">
+        <h3>Decorated Cakes ({cakeType})</h3>
+        <p style={textStyle}>
+          Select white, chocolate, or carrot to see pricing for standard cake
+          sizes. Custom decoration is available at an additional charge. Call or
+          email for a custom decoration quote.
+        </p>
+        <ButtonGroup size="large">
+          <Button style={whiteButtonStyle} onClick={() => setCakeType("white")}>
+            White
+          </Button>
+          <Button
+            style={chocButtonStyle}
+            onClick={() => setCakeType("chocolate")}
+          >
+            Chocolate
+          </Button>
+          <Button
+            style={carrotButtonStyle}
+            onClick={() => setCakeType("carrot")}
+          >
+            Carrot
+          </Button>
+        </ButtonGroup>
+        <div className="dec-cakes-prices-container">
+          {cakes.map((cake) => (
+            <div className="decorated-cake-card">
+              <div className="cake-card-label" style={labelStyle}>
+                <h5>{cake.title}</h5>
+                <span>{cake.serving}</span>
+              </div>
+              <div className="cake-price" style={priceContainerStyle}>
+                <span className="currency">$</span>
+                <span>{selectCakePrice(cake)}</span>
+              </div>
             </div>
-            <div className="decorated-cakes-container">
-                <h3>Decorated Cakes ({cakeType})</h3>
-                <p style={textStyle}>Select white, chocolate, or carrot to see pricing for standard cake sizes. Custom decoration is available at an additional charge. Call or email for a custom decoration quote.</p>
-                <ButtonGroup size="large">
-                    <Button style={whiteButtonStyle} onClick={() => setCakeType('white')}>White</Button>
-                    <Button style={chocButtonStyle} onClick={() => setCakeType('chocolate')}>Chocolate</Button>
-                    <Button style={carrotButtonStyle} onClick={() => setCakeType('carrot')}>Carrot</Button>
-                </ButtonGroup>
-                <div className="dec-cakes-prices-container">
-                    {cakes.map(cake => (
-                    <div className="decorated-cake-card">
-                        <div className="cake-card-label" style={labelStyle}>
-                            <h5>{cake.title}</h5>
-                            <span>{cake.serving}</span>
-                        </div>
-                        <div className="cake-price" style={priceContainerStyle}>
-                            <span className="currency">$</span>
-                            <span>{selectCakePrice(cake)}</span>
-                        </div>
-                    </div> 
-                    ))}
-                </div>
-            </div>
-            
-            <div className="standard-cakes-container">
-                <h3>8" Two Layer Cakes</h3>
-                <p style={textStyle}>Cake photos coming soon! In the meantime, please see our <Link to="/gallery">gallery</Link> for custom dessert photos</p>
-                <div className="standard-cakes-list">
-                    <ul>
-                        <li>
-                            <strong>Vanilla</strong>
-                            <span> - $17.50</span>
-                        </li>
-                        <li>
-                            <strong>Carrot</strong>
-                            <span> - $20.50</span>
-                        </li>
-                        <li>
-                            <strong>Cherry</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Chocolate</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Checkerboard</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Coconut</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Lemon</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Marble</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Orange</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Yellow</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Strawberry</strong>
-                            <span> - $18.50</span>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <strong>Black Forest</strong>
-                            <span> - $21.50</span>
-                        </li>
-                        <li>
-                            <strong>German Chocolate</strong>
-                            <span> - $21.50</span>
-                        </li>
-                        <li>
-                            <strong>Italian Cream</strong>
-                            <span> - $21.50</span>
-                        </li>
-                        <li>
-                            <strong>Chocolate Fudge</strong>
-                            <span> - $21.50</span>
-                        </li>
-                        <li>
-                            <strong>Chocolate Mousse</strong>
-                            <span> - $19.50</span>
-                        </li>
-                        <li>
-                            <strong>Chocolate Rasberry</strong>
-                            <span> - $19.50</span>
-                        </li>
-                        <li>
-                            <strong>Pina Colada</strong>
-                            <span> - $18.50</span>
-                        </li>
-                        <li>
-                            <strong>Red Velvet</strong>
-                            <span> - $19.50</span>
-                        </li>
-                        <li>
-                            <strong>Strawberry Shortcake</strong>
-                            <span> - $21.50</span>
-                        </li>
-                        <li>
-                            <strong>Tiramisu</strong>
-                            <span> - $24.50</span>
-                        </li>
-                        <li>
-                            <strong>Tres Leches</strong>
-                            <span> - $22.50</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
-}
+          ))}
+        </div>
+      </div>
 
-export default Cakes
- 
+      <div className="standard-cakes-container">
+        <h3>8" Two Layer Cakes</h3>
+        <p style={textStyle}>
+          Cake photos coming soon! In the meantime, please see our{" "}
+          <Link to="/gallery">gallery</Link> for custom dessert photos
+        </p>
+        <div className="standard-cakes-list">
+          <ul>
+            <li>
+              <strong>Vanilla</strong>
+              <span> - $19.95</span>
+            </li>
+            <li>
+              <strong>Carrot</strong>
+              <span> - $21.95</span>
+            </li>
+            <li>
+              <strong>Cherry</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Chocolate</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Checkerboard</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Coconut</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Lemon</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Marble</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Orange</strong>
+              <span> - $21.95</span>
+            </li>
+            <li>
+              <strong>Yellow</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Strawberry</strong>
+              <span> - $20.95</span>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <strong>Black Forest</strong>
+              <span> - $24.95</span>
+            </li>
+            <li>
+              <strong>German Chocolate</strong>
+              <span> - $22.95</span>
+            </li>
+            <li>
+              <strong>Italian Cream</strong>
+              <span> - $22.95</span>
+            </li>
+            <li>
+              <strong>Chocolate Fudge</strong>
+              <span> - $22.95</span>
+            </li>
+            <li>
+              <strong>Chocolate Mousse</strong>
+              <span> - $24.95</span>
+            </li>
+            <li>
+              <strong>Chocolate Rasberry</strong>
+              <span> - $22.95</span>
+            </li>
+            <li>
+              <strong>Pina Colada</strong>
+              <span> - $20.95</span>
+            </li>
+            <li>
+              <strong>Red Velvet</strong>
+              <span> - $22.95</span>
+            </li>
+            <li>
+              <strong>Strawberry Shortcake</strong>
+              <span> - $24.95</span>
+            </li>
+            <li>
+              <strong>Tiramisu</strong>
+              <span> - $26.95</span>
+            </li>
+            <li>
+              <strong>Tres Leches</strong>
+              <span> - $26.95</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
+export default Cakes;
--- src/img/index.js
+++ src/img/index.js
@@ -1,156 +1,194 @@
-const cakes = ["./img/cakes/cake-1.jpg", "./img/cakes/cake-2.jpg", "./img/cakes/cake-3.jpg", "./img/cakes/cake-4.jpg", "./img/cakes/cake-5.jpg", "./img/cakes/cake-6.jpg", "./img/cakes/cake-7.jpg", "./img/cakes/cake-8.jpg", "./img/cakes/cake-9.jpg", "./img/cakes/cake-10.jpg", "./img/cakes/cake-11.jpg", "./img/cakes/cake-12.jpg", "./img/cakes/cake-13.jpg", "./img/cakes/cake-14.jpg"]
+const cakes = [
+  "./img/cakes/cake-1.jpg",
+  "./img/cakes/cake-2.jpg",
+  "./img/cakes/cake-3.jpg",
+  "./img/cakes/cake-4.jpg",
+  "./img/cakes/cake-5.jpg",
+  "./img/cakes/cake-6.jpg",
+  "./img/cakes/cake-7.jpg",
+  "./img/cakes/cake-8.jpg",
+  "./img/cakes/cake-9.jpg",
+  "./img/cakes/cake-10.jpg",
+  "./img/cakes/cake-11.jpg",
+  "./img/cakes/cake-12.jpg",
+  "./img/cakes/cake-13.jpg",
+  "./img/cakes/cake-14.jpg",
+];
 
-const cookies = ["./img/cookies/cookie-1.jpg", "./img/cookies/cookie-2.jpg", "./img/cookies/cookie-3.jpg", "./img/cookies/cookie-4.jpg", "./img/cookies/cookie-5.jpg", "./img/cookies/cookie-6.jpg", "./img/cookies/cookie-7.jpg", "./img/cookies/cookie-8.jpg"]
+const cookies = [
+  "./img/cookies/cookie-1.jpg",
+  "./img/cookies/cookie-2.jpg",
+  "./img/cookies/cookie-3.jpg",
+  "./img/cookies/cookie-4.jpg",
+  "./img/cookies/cookie-5.jpg",
+  "./img/cookies/cookie-6.jpg",
+  "./img/cookies/cookie-7.jpg",
+  "./img/cookies/cookie-8.jpg",
+];
 
-const cupcakes = ["./img/cupcakes/cupcake-1.jpg", "./img/cupcakes/cupcake-2.jpg", "./img/cupcakes/cupcake-3.jpg", "./img/cupcakes/cupcake-4.jpg", "./img/cupcakes/cupcake-5.jpg", "./img/cupcakes/cupcake-6.jpg", "./img/cupcakes/cupcake-7.jpg", "./img/cupcakes/cupcake-8.jpg", "./img/cupcakes/cupcake-9.jpg", "./img/cupcakes/cupcake-10.jpg"]
+const cupcakes = [
+  "./img/cupcakes/cupcake-1.jpg",
+  "./img/cupcakes/cupcake-2.jpg",
+  "./img/cupcakes/cupcake-3.jpg",
+  "./img/cupcakes/cupcake-4.jpg",
+  "./img/cupcakes/cupcake-5.jpg",
+  "./img/cupcakes/cupcake-6.jpg",
+  "./img/cupcakes/cupcake-7.jpg",
+  "./img/cupcakes/cupcake-8.jpg",
+  "./img/cupcakes/cupcake-9.jpg",
+  "./img/cupcakes/cupcake-10.jpg",
+];
 
-const others = ["./img/otherDesserts/other-1.jpg", "./img/otherDesserts/other-2.jpg","./img/otherDesserts/other-3.jpg","./img/otherDesserts/other-4.jpg","./img/otherDesserts/other-5.jpg","./img/otherDesserts/other-6.jpg","./img/otherDesserts/other-7.jpg","./img/otherDesserts/other-8.jpg"]
+const others = [
+  "./img/otherDesserts/other-1.jpg",
+  "./img/otherDesserts/other-2.jpg",
+  "./img/otherDesserts/other-3.jpg",
+  "./img/otherDesserts/other-4.jpg",
+  "./img/otherDesserts/other-5.jpg",
+  "./img/otherDesserts/other-6.jpg",
+  "./img/otherDesserts/other-7.jpg",
+  "./img/otherDesserts/other-8.jpg",
+];
 
 const products = [
-    {
-        id: "pie-1",
-        name: 'Apple',
-        price: '$13.50',
-        photo: "./img/pies/apple-pie.jpg",
-    },
-    {
-        id: "pie-2",
-        name: 'Black Bottom',
-        price: '$13.50',
-        photo: "./img/pies/blackbottom-pie.jpg"
-    },
-    {
-        id: "pie-3",
-        name: 'Blueberry',
-        price: '$14.50',
-        photo: "./img/pies/blueberry-pie.jpg"
-    },
-    {
-        id: "pie-4",
-        name: 'Boston Cream',
-        price: '$13.50',
-        photo: "./img/pies/bostoncreme-pie.jpg"
-    },
-    {
-        id: "pie-5",
-        name: 'Bourbon Pecan',
-        price: '$16.50',
-        photo: "./img/pies/bourbonpecan-pie.jpg"
-    },
-    {
-        id: "pie-6",
-        name: 'Buttermilk',
-        price: '$13.50',
-        photo: "./img/pies/buttermilk-pie.jpg"
-    },  
-    {
-        id: "pie-7",
-        name: 'Cherry',
-        price: '$14.50',
-        photo: "./img/pies/cherry-pie.jpg"
-    },  
-    {
-        id: "pie-8",
-        name: 'Chocolate Chess',
-        price: '$13.50',
-        photo: "./img/pies/chocolatechess-pie.jpg"
-    },  
-    {
-        id: "pie-9",
-        name: 'Chocolate Meringue',
-        price: '$13.50',
-        photo: "./img/pies/chocolatemeringue-pie.jpg"
-    },  
-    {
-        id: "pie-10",
-        name: 'Coconute Chess',
-        price: '$13.50',
-        photo: "./img/pies/coconutchess-pie.jpg"
-    },  
-    {
-        id: "pie-11",
-        name: 'Coconut Cream',
-        price: '$13.50',
-        photo: "./img/pies/coconutcream-pie.jpg"
-    },  
-    {
-        id: "pie-12",
-        name: 'Coconut Meringue',
-        price: '$13.50',
-        photo: "./img/pies/coconutmeringue-pie.jpg"
-    },  
-    {
-        id: "pie-13",
-        name: 'Dutch Blueberry',
-        price: '$14.50',
-        photo: "./img/pies/dutchblueberry-pie.jpg"
-    },  
-    {
-        id: "pie-14",
-        name: 'Egg Custard',
-        price: '$13.50',
-        photo: "./img/pies/eggcustard-pie.jpg"
-    },  
-    {
-        id: "pie-15",
-        name: 'Key Lime',
-        price: '$17.50',
-        photo: "./img/pies/keylime-pie.jpg"
-    },  
-    {
-        id: "pie-16",
-        name: 'Lemon Chess',
-        price: '$13.50',
-        photo: "./img/pies/lemonchess-pie.jpg"
-    },  
-    {
-        id: "pie-17",
-        name: 'Lemon Meringue',
-        price: '$13.50',
-        photo: "./img/pies/lemonmeringue-pie.jpg"
-    },  
-    {
-        id: "pie-18",
-        name: 'Pecan',
-        price: '$15.50',
-        photo: "./img/pies/pecan-pie.jpg"
-    },  
-    {
-        id: "pie-19",
-        name: 'Pumpkin',
-        price: '$13.50',
-        photo: "./img/pies/pumpkin-pie.jpg"
-    },  
-    {
-        id: "pie-20",
-        name: 'Pumpkin Cheesecake',
-        price: '$14.50',
-        photo: "./img/pies/pumpkincheese-pie.jpg"
-    },  
-    {
-        id: "pie-21",
-        name: 'Strawberry',
-        price: '$17.50',
-        photo: "./img/pies/strawberry-pie.jpg"
-    },  
-    {
-        id: "pie-22",
-        name: 'Strawberry Rhubarb',
-        price: '$17.50',
-        photo: "./img/pies/strawberryrhubarb-pie.jpg"
-    },  
-    {
-        id: "pie-23",
-        name: 'Sweet Potato',
-        price: '$14.50',
-        photo: "./img/pies/swtpotato-pie.jpg"
-    },  
+  {
+    id: "pie-1",
+    name: "Apple",
+    price: "$14.50",
+    photo: "./img/pies/apple-pie.jpg",
+  },
+  {
+    id: "pie-2",
+    name: "Black Bottom",
+    price: "$14.50",
+    photo: "./img/pies/blackbottom-pie.jpg",
+  },
+  {
+    id: "pie-3",
+    name: "Blueberry",
+    price: "$15.50",
+    photo: "./img/pies/blueberry-pie.jpg",
+  },
+  {
+    id: "pie-4",
+    name: "Boston Cream",
+    price: "$14.50",
+    photo: "./img/pies/bostoncreme-pie.jpg",
+  },
+  {
+    id: "pie-5",
+    name: "Bourbon Pecan",
+    price: "$17.50",
+    photo: "./img/pies/bourbonpecan-pie.jpg",
+  },
+  {
+    id: "pie-6",
+    name: "Buttermilk",
+    price: "$14.50",
+    photo: "./img/pies/buttermilk-pie.jpg",
+  },
+  {
+    id: "pie-7",
+    name: "Cherry",
+    price: "$15.50",
+    photo: "./img/pies/cherry-pie.jpg",
+  },
+  {
+    id: "pie-8",
+    name: "Chocolate Chess",
+    price: "$14.50",
+    photo: "./img/pies/chocolatechess-pie.jpg",
+  },
+  {
+    id: "pie-9",
+    name: "Chocolate Meringue",
+    price: "$14.50",
+    photo: "./img/pies/chocolatemeringue-pie.jpg",
+  },
+  {
+    id: "pie-10",
+    name: "Coconute Chess",
+    price: "$14.50",
+    photo: "./img/pies/coconutchess-pie.jpg",
+  },
+  {
+    id: "pie-11",
+    name: "Coconut Cream",
+    price: "$14.50",
+    photo: "./img/pies/coconutcream-pie.jpg",
+  },
+  {
+    id: "pie-12",
+    name: "Coconut Meringue",
+    price: "$14.50",
+    photo: "./img/pies/coconutmeringue-pie.jpg",
+  },
+  {
+    id: "pie-13",
+    name: "Dutch Blueberry",
+    price: "$15.50",
+    photo: "./img/pies/dutchblueberry-pie.jpg",
+  },
+  {
+    id: "pie-14",
+    name: "Egg Custard",
+    price: "$14.50",
+    photo: "./img/pies/eggcustard-pie.jpg",
+  },
+  {
+    id: "pie-15",
+    name: "Key Lime",
+    price: "$18.50",
+    photo: "./img/pies/keylime-pie.jpg",
+  },
+  {
+    id: "pie-16",
+    name: "Lemon Chess",
+    price: "$14.50",
+    photo: "./img/pies/lemonchess-pie.jpg",
+  },
+  {
+    id: "pie-17",
+    name: "Lemon Meringue",
+    price: "$14.50",
+    photo: "./img/pies/lemonmeringue-pie.jpg",
+  },
+  {
+    id: "pie-18",
+    name: "Pecan",
+    price: "$16.50",
+    photo: "./img/pies/pecan-pie.jpg",
+  },
+  {
+    id: "pie-19",
+    name: "Pumpkin",
+    price: "$14.50",
+    photo: "./img/pies/pumpkin-pie.jpg",
+  },
+  {
+    id: "pie-20",
+    name: "Pumpkin Cheesecake",
+    price: "$15.50",
+    photo: "./img/pies/pumpkincheese-pie.jpg",
+  },
+  {
+    id: "pie-21",
+    name: "Strawberry",
+    price: "$18.50",
+    photo: "./img/pies/strawberry-pie.jpg",
+  },
+  {
+    id: "pie-22",
+    name: "Strawberry Rhubarb",
+    price: "$18.50",
+    photo: "./img/pies/strawberryrhubarb-pie.jpg",
+  },
+  {
+    id: "pie-23",
+    name: "Sweet Potato",
+    price: "$15.50",
+    photo: "./img/pies/swtpotato-pie.jpg",
+  },
+];
 
-]
-
-
-export { cakes, cookies, cupcakes, others, products }
-
-
-
-
+export { cakes, cookies, cupcakes, others, products };
